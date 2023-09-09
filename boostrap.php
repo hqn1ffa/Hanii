824a3162478fe79f77e7563ffdb04ef88eaf2c1f
hqn1ffa <144515064+hqn1ffa@users.noreply.github.com>
2023-09-09 13:13:21 +0700

    Add files via upload

---
+++ boostrap.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+
+ <style>
+  .bordered {
+  border: solid;
+}
+
+ .contoh { 
+  height: 50px;
+ background-color: skyblue;
+}
+</style>
+  <body>
+
+   <div class="container-fluid bordered">
+    <div class="row">
+     <div class="col-1">
+     <div class="contoh">1</div>
+</div>
+     <div class="col-1">
+       <div class="contoh">2</div>
+     
+</div>
+      <div class="col-1">
+<div class="contoh">3</div>
+</div>
+     <div class="col-1">
+       <div class="contoh">4</div>
+</div>
+     <div class="col-1">
+       <div class="contoh">5</div>
+</div>
+
+      <div class="col-1">
+       <div class="contoh">6</div>
+</div> 
+     <div class="col-1">
+       <div class="contoh">7</div>
+</div>
+     <div class="col-1">
+       <div class="contoh">8</div>
+</div>
+     <div class="col-1">
+       <div class="contoh">9</div>
+</div>
+     <div class="col-1">
+     
+      <div class="contoh">10</div>
+</div>     
+ <div class="col-1">
+  <div class="contoh">11</div>
+</div>
+      <div class="col-1">
+       <div class="contoh">12</div>
+</div>
+
+</div>
+
+
+
+
+    <h1>puput melinda</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
